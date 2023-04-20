f55e359c6e5a11cc5962b7b2c26e5f9ddc472a7c
Joakim <wahlstrom.joakim@gmail.com>
2023-04-20 15:19:26 +0200

    lektion 2

--- lektion-2/2-loops/src/components/Post/Post.jsx
+++ lektion-2/2-loops/src/components/Post/Post.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import './Post.css'
 
 const Post = ({post}) => {
+  const [isDark, setIsDark] = useState(false)
   return (
     <div className='Post'>
-      <div className='card mb-1'>
+      <div className={`card mb-1 ${isDark ? 'dark' : ''}`}>
         <h2>{post.title}</h2>
         <p>{post.body}</p>
+        <button onClick={() => setIsDark(prev => !prev)}>{isDark ? 'Light' : 'Dark'}</button>
       </div>
     </div>
   )
