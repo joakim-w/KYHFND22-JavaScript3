adf5985955f6e738933d8e7d38f2e61a7a928669
Joakim <wahlstrom.joakim@gmail.com>
2023-05-11 11:21:05 +0200

    lektion 8

--- lektion-8/2-redux-toolkit-thunk/src/app/store.js
+++ lektion-8/2-redux-toolkit-thunk/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import postsReducer from '../features/posts/postsSlice'
+import singlePostReducer from '../features/posts/singlePostSlice'
 
 export const store = configureStore({
   reducer: {
-    posts: postsReducer
+    posts: postsReducer, 
+    singlePost: singlePostReducer
   }
 })
---
+++ lektion-8/2-redux-toolkit-thunk/src/features/posts/singlePostSlice.js
@@ -0,0 +1,44 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import postsService from "./postsService"
+
+const initialState = {
+  post: null,
+  error: null,
+  loading: false
+}
+
+
+export const getPostById = createAsyncThunk('singlePost/getById', async (postId, thunkAPI) => {
+  try {
+    return await postsService.getByIdAsync(postId)
+  } catch (err) {
+    console.log(err.message)
+    return thunkAPI.rejectWithValue(err.message)
+  }
+})
+
+
+export const singlePostSlice = createSlice({
+  name: 'singlePost',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPostById.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(getPostById.fulfilled, (state, action) => {
+        state.loading = false
+        state.post = action.payload
+        state.error = null
+      })
+      .addCase(getPostById.rejected, (state, action) => {
+        state.loading = false
+        state.post = null
+        state.error = action.payload
+      })
+  }
+})
+
+
+export default singlePostSlice.reducer
