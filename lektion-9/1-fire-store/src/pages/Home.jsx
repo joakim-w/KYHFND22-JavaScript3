30170e95994bfcce9d618d7dd19ad810941a4405
joakim w <joakim.wahlstrom@epndata.com>
2023-05-16 13:36:54 +0200

    lektion 9

--- lektion-9/1-fire-store/src/pages/Home.jsx
+++ lektion-9/1-fire-store/src/pages/Home.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getProducts } from '../store/features/products/productListSlice'
 
 const Home = () => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getProducts())
+  }, [])
+
   return (
     <div>Home</div>
   )
