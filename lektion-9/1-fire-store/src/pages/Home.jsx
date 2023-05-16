e61ebbf944cbeceea112a15916b5e4f90234c920
joakim w <joakim.wahlstrom@epndata.com>
2023-05-16 14:08:58 +0200

    lektion 9

--- lektion-9/1-fire-store/src/pages/Home.jsx
+++ lektion-9/1-fire-store/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../store/features/products/productListSlice'
+import Loader from '../components/Loader/Loader'
+import ProductCard from '../components/ProductCard/ProductCard'
 
 const Home = () => {
 
@@ -10,8 +12,18 @@ const Home = () => {
     dispatch(getProducts())
   }, [])
 
+  const { products, loading, error } = useSelector(state => state.productList)
+
   return (
-    <div>Home</div>
+    <div>
+      { loading && <Loader />}
+      { error && <p>{error}</p>}
+      <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mt-5">
+        {
+          products.map(product => <ProductCard key={product.id} product={product} />)
+        }
+      </div>
+    </div>
   )
 }
 
