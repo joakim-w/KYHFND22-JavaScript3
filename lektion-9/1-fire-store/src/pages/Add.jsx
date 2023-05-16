97cb62f5658c6d4084e1e167b65fbb230df8675f
joakim w <joakim.wahlstrom@epndata.com>
2023-05-16 12:12:08 +0200

    lektion 9

--- lektion-9/1-fire-store/src/pages/Add.jsx
+++ lektion-9/1-fire-store/src/pages/Add.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-
+import { useDispatch } from 'react-redux'
+import { addProduct } from '../store/features/products/productListSlice'
 const Add = () => {
 
-  const [formData, setFormData] = useState({
+  const dispatch = useDispatch()
+  const [productData, setProductData] = useState({
     name: '',
     price: '',
     imageURL: '',
@@ -11,7 +13,7 @@ const Add = () => {
 
   const handleChange = e => {
     const { id, value } = e.target
-    setFormData(form => {
+    setProductData(form => {
       return {
         ...form,
         [id]: value
@@ -19,25 +21,35 @@ const Add = () => {
     })
   }
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    const data = {
+      ...productData,
+      price: +productData.price
+    }
+
+    dispatch(addProduct(data))
+  }
+
   return (
     <div>
       <h1 className='text-center my-5'>Add a new product</h1>
-      <form noValidate>
+      <form noValidate onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="name" className="form-label">Product Name:</label>
-          <input type="text" className="form-control" id='name' value={formData.name} onChange={handleChange} />
+          <input type="text" className="form-control" id='name' value={productData.name} onChange={handleChange} />
         </div>
         <div className="mb-3">
           <label htmlFor="price" className="form-label">Product Price:</label>
-          <input type="number" inputMode='decimal' className="form-control" id='price' value={formData.price} onChange={handleChange} />
+          <input type="number" inputMode='decimal' className="form-control" id='price' value={productData.price} onChange={handleChange} />
         </div>
         <div className="mb-3">
           <label htmlFor="imageURL" className="form-label">Image Url:</label>
-          <input type="text" className="form-control" id='imageURL' value={formData.imageURL} onChange={handleChange} />
+          <input type="text" className="form-control" id='imageURL' value={productData.imageURL} onChange={handleChange} />
         </div>
         <div className="mb-3">
           <label htmlFor="description" className="form-label">Product Description:</label>
-          <textarea className='form-control' id="description" rows="3" value={formData.description} onChange={handleChange}></textarea>
+          <textarea className='form-control' id="description" rows="3" value={productData.description} onChange={handleChange}></textarea>
         </div>
         <button className="btn btn-primary">Add Product</button>
       </form>
--- lektion-9/1-fire-store/src/store/features/products/productListSlice.js
+++ lektion-9/1-fire-store/src/store/features/products/productListSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import productsService from "./productsService";
 
 const initialState = {
   products: [],
@@ -6,11 +7,33 @@ const initialState = {
   loading: false
 }
 
+export const addProduct = createAsyncThunk('product-list/add', async (productData, thunkAPI) => {
+  try {
+    return await productsService.createProduct(productData)
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err.message)
+  }
+})
+
 export const productListSlice = createSlice({
   name: 'Product-list',
   initialState,
   reducers: {},
-  extraReducers: () => {}
+  extraReducers: (builder) => {
+    builder
+      .addCase(addProduct.pending, (state) => {
+        state.loading = true
+      })
+      .addCase(addProduct.fulfilled, (state, action) => {
+        state.loading = false
+        state.error = null
+        state.products = [...state.products, action.payload]
+      })
+      .addCase(addProduct.rejected, (state, action) => {
+        state.loading = false
+        state.error = action.payload
+      })
+  }
 })
 
 export default productListSlice.reducer
